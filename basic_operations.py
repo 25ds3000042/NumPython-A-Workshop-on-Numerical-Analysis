c7421e9fc3f764454efee4d209abee0f54dca5cc
bss1211 <mail.bhupendra.sidar.com>
2026-02-10 23:11:54 +0530

    operations

--- basic_operations.py
+++ basic_operations.py
@@ -3,3 +3,14 @@ x = 2.5
 y = 3.5
 
 print(x + y)
+
+a = 10
+b = 3
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Power:", a ** b)
